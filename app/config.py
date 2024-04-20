83904b180091d95b3bf7f0cb4a61929d89a853c4
wawrzu <kamwawrzak@gmail.com>
2024-04-20 18:27:00 +0200

    add jwt auth

--- app/config.py
+++ app/config.py
@@ -11,6 +11,7 @@ class Config:
         db_password = self.get_env_var('DB_PASSWORD')
         aws_key = self.get_env_var('AWS_ACCESS_KEY_ID')
         aws_secret = self.get_env_var('AWS_SECRET_ACCESS_KEY')
+        jwt_secret = self.get_env_var('JWT_SECRET')
 
         with open(file, 'r') as file:
             config = yaml.safe_load(file)
@@ -18,11 +19,12 @@ class Config:
             self.db_host = config['database']['host']
             self.db_port = config['database']['port']
             self.db_user = config['database']['user']
-            self.db_password =  db_password if db_password != "" else config['database']['password']
+            self.db_password =  db_password if db_password else config['database']['password']
             self.db_database = config['database']['database']
             self.aws_host = config['aws']['host']
-            self.aws_key_id = aws_key if aws_key != "" else config['aws']['aws_access_key_id']
-            self.aws_access_key = aws_secret if aws_secret != "" else config['aws']['aws_secret_access_key']
+            self.aws_key_id = aws_key if aws_key else config['aws']['aws_access_key_id']
+            self.aws_access_key = aws_secret if aws_secret else config['aws']['aws_secret_access_key']
+            self.jwt_secret =  jwt_secret if jwt_secret else config['jwt']['secret']
 
     def get_env_var(self, name: str):
        return os.environ[name]
